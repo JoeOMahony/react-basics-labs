026563e6c0d7a6a560605759cccbaf81d09a4422
Joe O'Mahony <21788075@setu.ie>
2026-09-16 11:48:26 +0100

    Rendered list of tasks using map()

--- taskly/src/App.jsx
+++ taskly/src/App.jsx
@@ -14,9 +14,13 @@ const [ taskState, setTaskState ] = useState({
   return (
     <div className="container">
       <h1>Taskly</h1>
-      <Task title={taskState.tasks[0].title} description={taskState.tasks[0].description} deadline={taskState.tasks[0].deadline} />
-      <Task title={taskState.tasks[1].title} description={taskState.tasks[1].description} deadline={taskState.tasks[1].deadline} />
-      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} />
+      {taskState.tasks.map((task) => (
+        <Task
+          title={task.title}
+          description={task.description}
+          deadline={task.deadline}
+        />
+      ))}
     </div>
   );
 }
